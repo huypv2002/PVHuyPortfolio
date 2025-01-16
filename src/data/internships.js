d8ba7c01c63cd43efce33c83acde0a8c8ddf6420
Yash Kamble <yashkamble1234567890@gmail.com>
2025-01-16 23:46:48 +0530

    Update internships.js

--- src/data/internships.js
+++ src/data/internships.js
@@ -1,5 +1,54 @@
 const internships = [
   {
+    company: "Software Developer Advance Computing",
+    location: "Mumbai",
+    duration: "December 2024 - Februaru 2025",
+    mode: "On-Site",
+    projects: [
+      {
+        name: "Netherlands Port Project",
+        description:
+          "A dynamic website for a Netherlands port, built using HTML, Bootstrap, Java, and SQL to manage port operations, including vessel tracking, cargo management, and logistics. The site offers a responsive UI, efficient backend integration, and smooth data handling.",
+      },
+    ],
+    description:
+      "Built a software for the Netherlands Port Project using HTML, Bootstrap, Java, and SQL",
+    skills: [
+      {
+        name: "Java",
+        color: "bg-amber-200",
+        textColor: "text-amber-800",
+        darkBgColor: "dark:bg-amber-600",
+        darkTextColor: "dark:text-amber-200",
+        logo: "https://res.cloudinary.com/dlnvozmgw/image/upload/v1737047782/java_gyfz9z.png",
+      },
+      {
+        name: "SQL",
+        color: "bg-lime-200",
+        textColor: "text-lime-800",
+        darkBgColor: "dark:bg-lime-600",
+        darkTextColor: "dark:text-lime-200",
+        logo: "https://res.cloudinary.com/dlnvozmgw/image/upload/v1737047781/sql_zb4hoz.png",
+      },
+      {
+        name: "HTML5",
+        color: "bg-green-200",
+        textColor: "text-green-800",
+        darkBgColor: "dark:bg-green-600",
+        darkTextColor: "dark:text-green-200",
+        logo: "https://res.cloudinary.com/dlnvozmgw/image/upload/v1726791975/logo/html_pe402c.png",
+      },
+      {
+        name: "Bootstrap",
+        color: "bg-pink-200",
+        textColor: "text-pink-800",
+        darkBgColor: "dark:bg-pink-600",
+        darkTextColor: "dark:text-pink-200",
+        logo: "https://res.cloudinary.com/dlnvozmgw/image/upload/v1726791947/logo/bootstrap_vstpde.png",
+      },
+    ],
+  },
+  {
     company: "Datalabs Private Limited",
     location: "Bengaluru",
     duration: "November 2023 - December 2023",
@@ -8,37 +57,37 @@ const internships = [
       {
         name: "Online Hotel Booking System",
         description:
-          " Developed a structured framework for a hotel booking system, focusing on design and layout to streamline the reservation process. This project enhanced understanding of web development concepts and provided practical experience in implementing design principles.",
+          "Developed a structured framework for a hotel booking system, focusing on design and layout to streamline the reservation process. This project enhanced understanding of web development concepts and provided practical experience in implementing design principles.",
       },
     ],
     description:
       "Developed an online hotel booking system, significantly boosting HTML and web development skills.",
-      skills: [
-        {
-          name: "JavaScript",
-          color: "bg-blue-200",
-          textColor: "text-blue-800",
-          darkBgColor: "dark:bg-blue-600",
-          darkTextColor: "dark:text-blue-200",
-           logo: "https://res.cloudinary.com/dlnvozmgw/image/upload/v1726791971/logo/javascript_tlgwab.png",
-        },
-        {
-          name: "HTML5",
-          color: "bg-green-200",
-          textColor: "text-green-800",
-          darkBgColor: "dark:bg-green-600",
-          darkTextColor: "dark:text-green-200",
-           logo: "https://res.cloudinary.com/dlnvozmgw/image/upload/v1726791975/logo/html_pe402c.png",
-        },
-        {
-          name: "CSS3",
-          color: "bg-gray-200",
-          textColor: "text-gray-800",
-          darkBgColor: "dark:bg-gray-600",
-          darkTextColor: "dark:text-gray-200",
-           logo: "https://res.cloudinary.com/dlnvozmgw/image/upload/v1726791962/logo/css_ldx5n5.png",
-        },
-  ]
+    skills: [
+      {
+        name: "JavaScript",
+        color: "bg-blue-200",
+        textColor: "text-blue-800",
+        darkBgColor: "dark:bg-blue-600",
+        darkTextColor: "dark:text-blue-200",
+        logo: "https://res.cloudinary.com/dlnvozmgw/image/upload/v1726791971/logo/javascript_tlgwab.png",
+      },
+      {
+        name: "HTML5",
+        color: "bg-green-200",
+        textColor: "text-green-800",
+        darkBgColor: "dark:bg-green-600",
+        darkTextColor: "dark:text-green-200",
+        logo: "https://res.cloudinary.com/dlnvozmgw/image/upload/v1726791975/logo/html_pe402c.png",
+      },
+      {
+        name: "CSS3",
+        color: "bg-gray-200",
+        textColor: "text-gray-800",
+        darkBgColor: "dark:bg-gray-600",
+        darkTextColor: "dark:text-gray-200",
+        logo: "https://res.cloudinary.com/dlnvozmgw/image/upload/v1726791962/logo/css_ldx5n5.png",
+      },
+    ],
   },
   {
     company: "Intrainz Innovation Private Limited",
@@ -65,31 +114,31 @@ const internships = [
     description:
       "Worked on several projects, gaining hands-on JavaScript experience in an online environment.",
     skills: [
-          {
-            name: "JavaScript",
-            color: "bg-blue-200",
-            textColor: "text-blue-800",
-            darkBgColor: "dark:bg-blue-600",
-            darkTextColor: "dark:text-blue-200",
-             logo: "https://res.cloudinary.com/dlnvozmgw/image/upload/v1726791971/logo/javascript_tlgwab.png",
-          },
-          {
-            name: "HTML5",
-            color: "bg-green-200",
-            textColor: "text-green-800",
-            darkBgColor: "dark:bg-green-600",
-            darkTextColor: "dark:text-green-200",
-             logo: "https://res.cloudinary.com/dlnvozmgw/image/upload/v1726791975/logo/html_pe402c.png",
-          },
-          {
-            name: "CSS3",
-            color: "bg-gray-200",
-            textColor: "text-gray-800",
-            darkBgColor: "dark:bg-gray-600",
-            darkTextColor: "dark:text-gray-200",
-             logo: "https://res.cloudinary.com/dlnvozmgw/image/upload/v1726791962/logo/css_ldx5n5.png",
-          },
-    ]
+      {
+        name: "JavaScript",
+        color: "bg-blue-200",
+        textColor: "text-blue-800",
+        darkBgColor: "dark:bg-blue-600",
+        darkTextColor: "dark:text-blue-200",
+        logo: "https://res.cloudinary.com/dlnvozmgw/image/upload/v1726791971/logo/javascript_tlgwab.png",
+      },
+      {
+        name: "HTML5",
+        color: "bg-green-200",
+        textColor: "text-green-800",
+        darkBgColor: "dark:bg-green-600",
+        darkTextColor: "dark:text-green-200",
+        logo: "https://res.cloudinary.com/dlnvozmgw/image/upload/v1726791975/logo/html_pe402c.png",
+      },
+      {
+        name: "CSS3",
+        color: "bg-gray-200",
+        textColor: "text-gray-800",
+        darkBgColor: "dark:bg-gray-600",
+        darkTextColor: "dark:text-gray-200",
+        logo: "https://res.cloudinary.com/dlnvozmgw/image/upload/v1726791962/logo/css_ldx5n5.png",
+      },
+    ],
   },
 ];
 
